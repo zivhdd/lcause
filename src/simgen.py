27c76a3d23daa7225c77ea8cd313da68348e18e6
Ziv Weiss Haddad <zivhdd@gmail.com>
2022-11-01 23:03:09 +0200

    simulation changes

--- src/simgen.py
+++ src/simgen.py
@@ -1,12 +1,6 @@
-
-
-from tabnanny import verbose
+import pickle, logging, json, random
 from event import Event
-import torch
-import random, heapq
-import pickle
-import logging
-import json
+from simulation import Reactor, Actor
 
 class LogType:
     START_NODES = 0
@@ -21,76 +15,6 @@ class LogType:
     UNEXPECTED_MESSAGE = 9
     STALE_FEED = 10
 
-class Reactor(object):
-    def __init__(self):
-        self.timers = []
-        self.time = 0
-        self.actors = []
-        self.logs = []
-
-    def notify(self, event, delay=0):
-        self.schedule(delay, lambda: self.notify_i(event))
-
-    def notify_i(self, event):
-        for actor in self.actors:
-            actor.handle(event)
-    
-    def log(self, logid, message, params=[]):
-        event =  Event(self.time, logid, params=params, text=message)
-        self.logs.append(event)
-        logging.debug(f"Event: {event}")
-
-    def schedule(self, delta, clb):
-        heapq.heappush(self.timers, (self.time + delta, clb))
-
-    def add(self, actor):
-        self.actors.append(actor)
-        actor.attach(len(self.actors), self)
-
-    def simulate(self, until_time):
-        while self.time < until_time and self.timers:
-            time, clb = heapq.heappop(self.timers)
-            self.time = time
-            clb()
-
-    def simulate_for(self, duration):
-        self.simulate(self.time + duration)
-            
-class Actor(object):
-
-    def __init__(self):
-        self.poll_mean = 1.0
-        self.poll_std = 0.2
-        self.poll_min = 0.5
-
-    def attach(self, id, reactor):
-        self.id = id
-        self.reactor = reactor
-        self.attached()
-        self.schedule_poll()
-
-    def rndtime(self, mean=1, std=1, min=0):
-        return max(torch.normal(self.poll_mean, self.poll_std, (1,)).tolist()[0], self.poll_min)
-
-    def withprob(self, prob):
-        return (random.random() < prob)
-
-    def schedule_poll(self):
-        interval = self.rndtime(self.poll_mean, self.poll_std, self.poll_min)
-        reactor.schedule(interval, self.poll)
-
-    def poll(self):
-        self.do_poll()
-        self.schedule_poll()
-
-    def do_poll(self):
-        pass
-
-    def attached(self):
-        pass
-
-    def handle(self, event):
-        pass
 
 class Trader(Actor):
 
@@ -168,7 +92,7 @@ class Operator(Actor):
             self.reactor.schedule(self.rndtime(60,30,10), self.start_all)
          
 
-class SimpleSim(Actor):
+class BallGameSim(Actor):
 
     def __init__(self):
         super().__init__()
@@ -227,7 +151,7 @@ if __name__ == '__main__':
         reactor.add(Trader())    
         reactor.add(Operator())
     else:
-        reactor.add(SimpleSim())
+        reactor.add(BallGameSim())
 
     reactor.simulate_for(args.period)   
 
