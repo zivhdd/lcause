826eba720a068d87209458c37212daf19836dc76
Ziv Weiss Haddad <zivhdd@gmail.com>
2022-11-01 23:31:57 +0200

    cleanify

--- src/main.py
+++ src/main.py
@@ -1,36 +1,41 @@
 
 ## replace with import
-import logging
+import os, logging
 from att import *
 import torch
 from event import Event, load_event_list
 from sampler import Sampler
 from model import  train_ext
 from att import EventPredictor
-from sim import LogType
+
 import logview
 from collections import defaultdict
 from samples import SAMPLES3
 
 logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.INFO)
 
-if False:
-    logs = load_event_list("ball.json")
+mode = "ball"
+logs = load_event_list(f"../data/{mode}.json")
+model_path = f"../models/{mode}.json"
+result_path = f"../results/{mode}.html"
+
+if mode == "ball":    
     window = 3
     num_any = 2
     weight_any = 8
     nconds=15
-else:
+elif mode == "trader":
     logs = load_event_list("trader.json")
     window = 180
     num_any = 2
     weight_any = 50
     nconds = 20
+else:
+    raise Exception("Unexpected mode")
 
 nevents = max([x.logid for x in logs]) + 1
 
 nparams = 0
-
 
 logging.info(f"nevents={nevents}; nconds={nconds}; nparms={nparams}")
 
@@ -40,46 +45,27 @@ sampler.config(num_any=num_any, weight_any=weight_any, with_positive=True)
 sampler.sample_any(torch.tensor([3]),1.0, 20)
 
 
-
-
-#print("####", sampler.sample(1))
-#model = EventPredictor(nevents=nevents)
-#model = SimplePred2(nevents=nevents, nconds=nconds) ##, emb_dim=40, depth=8)#, depth=6, emb_dim=6)
-#model = TransPred(nevents=nevents, nconds=nconds)
-#model = create_model(nevents=nevents, nparams=nparams, nconds=nconds)
 model = LinearPredExtended(nevents=nevents)
 
-if False:
-    obj = torch.load("model2.obj")
-    #obj["inner.0.weight"][:] = 0.0
-    #obj["inner.0.weight"][:, 6] = 6.0
-    #obj["inner.0.bias"][:] = -5.0
+
+if os.path.isfile(model_path):
+    obj = torch.load(model_path)
     model.load_state_dict(obj)
     logging.info("loaded model")
     
 
+def model_loss(model):
+    betas = model.betas[0:-1,:]
+    beta_loss = (betas.abs()).sum() * 0.001            
+    return beta_loss
 
-def dep(model, device, event, cond):
-    out = model.cond_filter(torch.tensor([[event, cond]], device=device), return_weight=True)
-    return (out[0,0].abs().tolist())
-
-
-def track(idx, model, device):
-    print("### C(TASSERT | REJECTED)", dep(model, device, LogType.TICKER_ASSERT, LogType.ORDER_REJECTED))
-    print("### C(REJECTED | TASSERT)", dep(model, device, LogType.ORDER_REJECTED, LogType.TICKER_ASSERT))
-    print("### C(STOP | TASSERT)", dep(model, device, LogType.STOP_NODES, LogType.TICKER_ASSERT))
-    tab = ([[dep(model, device, eventid, condid) for condid in range(nevents)] for eventid in range(nevents)])
-    print("\n".join([" ".join(map(lambda x: ("%5s" % f"{(x):.2f}"), ty)) for ty in tab]) )
-    #print(tab)
-    return True
 
 dlogs = [x.to_dict() for x in logs]
 
-DEBUG = False
 
 def compile(idx, model, device, window=window):
     model.eval()
-    torch.save(model.state_dict(), "model2.obj")
+    torch.save(model.state_dict(), model_path)
     logging.info(f"compiling {idx}")
     lpos = 0
     probs = defaultdict(list)
@@ -97,12 +83,11 @@ def compile(idx, model, device, window=window):
         condidx = [logs[x].logid for x in range(lpos, idx)]
 
         input = [([logs[idx].logid] + conds) for i in range(idx + 1 - lpos)]
-        #input = [([] + conds) for i in range(idx + 1 - lpos)]
+
         for ci in range(idx - lpos):
             input[ci + 1][ci + 1] = sampler.tok_no_event
         input = torch.tensor(input)
         values = model(input.to(device)) 
-        #print(input, values, sep="\n")
 
         baseval = values[0].tolist()
         probs[idx] = baseval
@@ -110,22 +95,11 @@ def compile(idx, model, device, window=window):
             altval = values[ci+1].tolist()
             causes[idx].append([lpos + ci, altval])
 
-        if DEBUG and idx == 9:
-            pdb.set_trace()
-    #print("ok")
-    logview.compile(dlogs, probs, causes, output = "progress.html")
+    logview.compile(dlogs, probs, causes, output = result_path)
     return True
 
-
-
-def count(aaa):
-    ddd=defaultdict(int)
-    for idx in aaa:
-        ddd[idx] += 1
-    return dict(ddd)
-
-train_ext(model, sampler, track=compile, batch_size=100, num_epochs=1000, lr=0.005, weight_decay=0)
-#print(count(torch.round(((sampler.rand_time(100000)+300)/1000)).int().tolist()))
+train_ext(model, sampler, track=compile, batch_size=100, num_epochs=1000, 
+    lr=0.005, weight_decay=0, model_loss=model_loss)
 
 
 
--- src/model.py
+++ src/model.py
@@ -7,45 +7,11 @@ import pdb
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-
-class Adaptor(nn.Module):
-    
-    def __init__(self, nevents=5, nconds=1, emb_dim=5, nparams=1):
-        super().__init__()
-        self.embedding = nn.Embedding(nevents+2, emb_dim)
-        self.nparams = nparams
-        self.output_size = emb_dim * 2
-
-    def forward(self, input):
-        item_size = 1 + self.nparams
-        event_emb = self.embedding(input[:,0].int())
-        cond_emb = self.embedding(input[:,item_size].int())
-        return torch.concat((event_emb, cond_emb), dim=1)
-
-class Residual(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.ff = nn.Sequential(*[nn.Linear(dim, dim), nn.ReLU()])
-
-    def forward(self, input):
-        return self.ff(input) + input
-
-
-def create_model(nevents=5, nconds=1, emb_dim=5, inter_dim=20, nparams=1, depth=8):
-    adaptor = Adaptor(nevents=nevents, nconds=nconds, emb_dim=emb_dim, nparams=nparams)
-    parts = (
-        [adaptor, nn.Linear(adaptor.output_size, inter_dim)] +
-        [Residual(inter_dim) for idx in range(depth)] +
-        [nn.Linear(inter_dim, 1), nn.Sigmoid()])
-    return nn.Sequential(*parts)
-
-
-def train_model(model, loss_fun, optimizer, sampler, repeats=100, batch_size=100, num_epochs=100, device=None, track=None):
+def train_model(model, loss_fun, optimizer, sampler, repeats=100, batch_size=100, 
+                num_epochs=100, device=None, track=None, model_loss=None):
     for idx in range(num_epochs):
 
         logging.info(f"{idx}")    
-        ok = 0
-        total =0 
         model.train()
         for rpt in range(repeats):
             data = sampler.sample(batch_size)
@@ -55,35 +21,22 @@ def train_model(model, loss_fun, optimizer, sampler, repeats=100, batch_size=100
             weight = data[:,-2]
             target = data[:,-1]
             optimizer.zero_grad()
-            #model.cond_filter.loss = None
-            y_model = model(input) 
-            loss = (loss_fun(y_model.flatten(), target) * weight).sum() / weight.sum() ##+ 0.0 *  model.cond_filter.loss
+            
+            y_model = model(input)            
+            loss = (loss_fun(y_model.flatten(), target) * weight).sum() / weight.sum()
 
-            betas = model.betas[0:-1,:]
-            beta_loss = (betas.abs()).sum() * 0.001            
-            loss += beta_loss
-            #for x in list(model.parameters())[0:1]:
-            #    loss += (x.data.abs()).sum() * 0.001
-            #loss += torch.sum(0.001 * (torch.concat([x. flatten() for x in model.chain.parameters()]).abs() ** 0.5))
-            #loss = loss_fun(y_model.flatten(), target) #+  0.0 *  model.cond_filter.loss            
+            if model_loss:
+                loss += model_loss(model)
+
             loss.backward()            
             optimizer.step()
-            
-            #pdb.set_trace()
-            ddd = data.cpu()
-            sss=ddd[ddd[:,0]==3,:]  
-            sss2=sss[(sss[:,1:-3] == 1).any(dim=1),:] 
-            ok += sss2[:,-1].sum()
-            total +=   sss2.shape[0]        
-        logging.info(f"P(2|0)={ok/total}")
-        #pdb.set_trace()
-
-        
+                    
         if track is not None and not track(idx, model, device):
             return
 
 
-def train_ext(model, sampler, device_name='cuda:0', batch_size=20, num_epochs=1000, lr=0.005, track=None, weight_decay=0.0001):    
+def train_ext(model, sampler, device_name='cuda:0', batch_size=20, num_epochs=1000, 
+    lr=0.005, track=None, weight_decay=0, **kwargs):    
     logging.info(f"start training")
     device = torch.device(device_name)
     if device is not None:
@@ -94,27 +47,8 @@ def train_ext(model, sampler, device_name='cuda:0', batch_size=20, num_epochs=10
         batch_size=batch_size,
         optimizer=optimizer, sampler=sampler, 
         device=device,num_epochs=num_epochs,
-        track = track)
+        track = track,
+        **kwargs)
+
     return model
 
-
-#############
-
-if False:
-    sampler = Sampler(SAMPLES)
-    model = create_model()
-
-    def mtrack(idx, model, device):
-        inp = torch.tensor([
-            [2,10,1,10],
-            [2,10,1,20],
-            [2,10,0,30],
-        ])
-        values = model(inp.to(device))
-        print(inp, values, sep="\n")
-        return True
-
-    #example = sampler.sample(1)
-    #aaa = model(example)
-    train_ext(model, sampler, track=mtrack)
-        
